3e1125b21fae5346d2c53e546125f7971e01d94e
Ria Das <riadas@mit.edu>
2021-01-25 17:36:36 -0500

    finished utils, progress on march.jl

--- src/march.jl
+++ src/march.jl
@@ -2,7 +2,7 @@ using GeometyBasics
 
 const Distance = Float64
 
-const Position  = Vec3
+const Position = Vec3
 const Direction = Vec3  ## Should be normalized. TODO: use a newtype wrapper
 
 const BlockHalfWidths = Vec3
@@ -60,3 +60,172 @@ struct Params
 end
 
 const Scene = Vector{Object}
+
+function sampleReflection(oriented_surface::OrientedSurface, ray::Ray, rng::AbstractRNG)::Ray 
+  nor, surf = oriented_surface
+  pos, dir = ray
+  if surf isa Matte
+    new_dir = sample_cosine_weighted_hemisphere(nor, rng)
+  else
+    new_dir = dir - (2.0 * dot(dir, nor)) * nor
+  end
+  (pos, new_dir)
+end
+
+function probReflection(oriented_surface::OrientedSurface, ray1::Ray, ray2::Ray)::Float 
+  nor, surf = oriented_surface
+  _, outRayDir = ray2
+  if surf isa Matte
+    relu(dor(nor, outRayDir))
+  else
+    0.0
+  end
+end
+
+function applyFilter(filter::Filter, radiance::Radiance)::Radiance 
+  
+end
+
+function surfaceFilter(filter::Filter, surf::Surface)::Filter 
+
+end
+
+function sdObject(pos::Position, obj::Object)::Distance 
+  if obj isa PassiveObject
+    object_geom = obj.object_geom
+    if object_geom isa Wall
+      nor = object_geom.direction
+      d = object_geom.distance
+      d + dot(nor, pos)
+    elseif object_geom isa Block
+      blockPos = object_geom.position
+      halfWidths = object_geom.blockhalfwidths
+      angle = object_geom.angle
+      
+      newPos = rotateY(pos - blockPos, angle)
+      length(map(relu, map(abs, newPos) - halfWidths))
+    else # object_geom isa Sphere
+      spherePos = object_geom.position
+      r = object_geom.radius
+
+      newPos = pos - spherePos
+      relu(length(newPos) - r)
+    end
+  else # obj isa Light
+    squarePos = object.position
+    hw = object.something
+
+    newPos = pos - squarePos
+    halfWidths = [hw, 0.01, hw]
+    length(map(relu, map(abs, newPos) - halfWidths))
+  end
+end
+
+function sdScene(scene::Scene, pos::Position) # (Object & Distance) 
+  
+end
+
+function calcNormal(obj::Object, pos::Position)::Direction 
+  normalize(grad(flip(sdObject, obj)) pos)
+end
+
+# ----- Start: Define RayMarchResult ----- #
+abstract type RayMarchResult end
+
+struct HitObj <: RayMarchResult
+  ray::Ray
+  oriented_surface::OrientedSurface
+end
+
+struct HitLight <: RayMarchResult
+  radiance::Radiance
+end
+
+struct HitNothing <: RayMarchResult end
+
+# ----- End: Define RayMarchResult ----- #
+
+function raymarch(scene::Scene, ray::Ray)::RayMarchResult 
+  maxIters = 100
+  tol = 0.01
+  startLength = 10.0 * tol # trying to escape the current surface
+  (rayOrigin, rayDir) = ray
+
+end
+
+function rayDirectRadiance(scene::Scene, ray::Ray)::Radiance 
+  result = raymarch(scene, ray)
+  if result isa HitLight
+    result.radiance
+  elseif result isa HitNothing
+    # color zero
+  else # result isa HitObj
+    # color zero
+  end
+end
+
+function sampleSquare(hw::Float, rng::AbstractRNG)::Position
+  x = rand_uniform(-hw, hw, rng)
+  z = rand_uniform(-hw, hw, rng)
+  [x, 0.0, z] 
+end
+
+function sampleLightRadiance(scene::Scene, osurf::OrientedSurface, inRay::Ray, rng::AbstractRNG)::Radiance 
+  surfNor, surf = osurf
+  rayPos, _ = inRay
+
+  #=
+  yieldAccum \radiance.
+    for i. case objs.i of
+      PassiveObject _ _ -> ()
+      Light lightPos hw _ ->
+        (dirToLight, distToLight) = directionAndLength $
+                                      lightPos + sampleSquare hw k - rayPos
+        if positiveProjection dirToLight surfNor then
+          -- light on this far side of current surface
+          fracSolidAngle = (relu $ dot dirToLight yHat) * sq hw / (pi * sq distToLight)
+          outRay = (rayPos, dirToLight)
+          coeff = fracSolidAngle * probReflection osurf inRay outRay
+          radiance += coeff .* rayDirectRadiance scene outRay
+  =#
+end
+
+function trace(params::Params, scene::Scene, initRay::Ray, rng::AbstractRNG)::Color 
+  noFilter = [1.0, 1.0, 1.0]
+  #=
+  yieldAccum \radiance.
+    runState  noFilter \filter.
+     runState initRay  \ray.
+      boundedIter (getAt #maxBounces params) () \i.
+        case raymarch scene $ get ray of
+          HitNothing -> Done ()
+          HitLight intensity ->
+            if i == 0 then radiance += intensity   -- TODO: scale etc
+            Done ()
+          HitObj incidentRay osurf ->
+            [k1, k2] = splitKey $ hash k i
+            lightRadiance = sampleLightRadiance scene osurf incidentRay k1
+            ray    := sampleReflection osurf incidentRay k2
+            filter := surfaceFilter (get filter) (snd osurf)
+            radiance += applyFilter (get filter) lightRadiance
+            Continue
+  =#
+end
+
+struct Camera 
+  numPix::Int
+  pos::Position
+  halfWidth::Float64
+  sensorDist::Float64
+end
+
+function cameraRays(n::Int, camera::Camera)::Matrix 
+
+end
+
+function takePicture(params::Params, scene::Scene, camera::Camera)::Image 
+
+end
+
+
+# TODO: Image? Matrix? flip? snd?
--- src/util.jl
+++ src/util.jl
@@ -2,46 +2,45 @@ using GeometyBasics
 using Distributions
 using LinearAlgebra: dot, cross
 
-relu(x) = max(x, 0.0)
-len(x) = sqrt(sum(x.^ 2))
-normalize(x) = x ./ len(x)
-direction_and_length(x) = (l = len(x); (x ./ l, x))
-rand_uniform(a, b, rng) = rand(rng, Uniform(a, b))
-sample_average(n, rng) = ..
-positive_projection(x, y) = dot(x, y) > 0.0
+relu(x::Float64) = max(x, 0.0)
+len(x::Array{Float64}) = sqrt(sum(x.^ 2))
+normalize(x::Array{Float64}) = x ./ len(x)
+direction_and_length(x::Array{Float64}) = (l = length(x); (x ./ l, l))
+rand_uniform(a::Float64, b::Float64, rng::AbstractRNG) = rand(rng, Uniform(a, b))
+sample_average(sample::Function, n::Int, rng::AbstractRNG) = sum([sample(rng) for i in 1:n])/n
+positive_projection(x::Array{Float}, y::Array{Float}) = dot(x, y) > 0.0
 
-xHat  = [1., 0., 0.]
-yHat  = [0., 1., 0.]
-zHat  = [0., 0., 1.]
+const xHat  = [1., 0., 0.]
+const yHat  = [0., 1., 0.]
+const zHat  = [0., 0., 1.]
 
-Angle = Float64
+const Angle = Float64
 
-function rotate_X(p, angle::Vec3)
+function rotate_X(p::Array{Float64}, angle::Angle)::Array{Float64}
   c = cos(angle)
   s = sin(angle)
   px, py, pz = p
-  Vec3(px, c*py - s*pz, s*py + c*pz)
+  [px, c*py - s*pz, s*py + c*pz]
 end
 
-function rotate_Y(p, angle::Vec3)
+function rotate_Y(p::Array{Float64}, angle::Angle)::Array{Float64}
   c = cos(angle)
   s = sin(angle)
   px, py, pz = p
-  Vec3(c*px + s*pz, py, - s*px+ c*pz)
+  [c*px + s*pz, py, - s*px+ c*pz]
 end
 
-function rotate_Z(p, angle::Vec3)
+function rotate_Z(p::Array{Float64}, angle::Angle)::Array{Float64}
   c = cos(angle)
   s = sin(angle)
   px, py, pz = p
-  Vec3(c*px - s*py, s*px+c*py, pz)
+  [c*px - s*py, s*px+c*py, pz]
 end
 
-function sample_cosine_weighted_hemisphere(normal::Vec3, k::Key)
-  k1, k2 = splitKey k
-  u1 = rand(k1)
-  u2 = rand(k2)
-  uu = normalize $ cross normal [0.0, 1.1, 1.1]
+function sample_cosine_weighted_hemisphere(normal::Array{Float64}, rng::AbstractRNG)::Array{Float64}
+  u1 = rand(rng)
+  u2 = rand(rng)
+  uu = normalize(cross(normal, [0.0, 1.1, 1.1]))
   vv = cross(uu, normal)
   ra = sqrt(u2)
   rx = ra * cos(2.0 * pi * u1)
